0178ca4adbd775e7caf21f7fdee628a3c4d02d2f
s4ze <imgood1104@gmail.com>
2024-02-10 19:02:18 +0300

    Создание страницы пользователя

---
+++ pages/users/[userId].tsx
@@ -0,0 +1,39 @@
+import { useRouter } from "next/router";
+import { ClipLoader } from "react-spinners";
+
+import Header from "@/components/Header";
+import UserHero from "@/components/users/UserHero";
+import useUser from "@/hooks/useUser";
+import UserBio from "@/components/UserBio";
+
+const UserView = () => {
+  const router = useRouter();
+  const { userId } = router.query;
+
+  const { data: fetchedUser, isLoading } = useUser(userId as string)
+
+  if (isLoading || !fetchedUser) {
+    return (
+      <div
+        className="
+          flex
+          justify-center
+          icons-center
+          h-full
+        "
+      >
+        <ClipLoader color='lightblue' size={80} />
+      </div>
+    )
+  }
+
+  return (
+    <>
+      <Header showBackArrow label={fetchedUser?.name} />
+      <UserHero userId={userId as string} />
+      <UserBio userId={userId as string} />
+    </>
+  );
+}
+
+export default UserView;
