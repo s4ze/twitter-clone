7acc10dd5d29938320d5d593c64bafa1fd1d1d82
s4ze <imgood1104@gmail.com>
2024-02-11 15:10:13 +0300

    Добавление загрузки изображений

--- components/modals/EditModal.tsx
+++ components/modals/EditModal.tsx
@@ -8,6 +8,7 @@ import useUser from "@/hooks/useUser";
 
 import Modal from "../Modal";
 import Input from "../Input";
+import ImageUpload from "../ImageUpload";
 
 const EditModal = () => {
   const { data: currentUser } = useCurrentUser()
@@ -62,6 +63,18 @@ const EditModal = () => {
 
   const bodyContent = (
     <div className="flex flex-col gap-4">
+      <ImageUpload
+        value={profileImage}
+        disabled={isLoading}
+        onChange={(image) => setProfileImage(image)}
+        label="Upload profile image"
+      />
+      <ImageUpload
+        value={coverImage}
+        disabled={isLoading}
+        onChange={(image) => setCoverImage(image)}
+        label="Upload cover image"
+      />
       <Input
         placeholder="Name"
         onChange={(e) => setName(e.target.value)}
