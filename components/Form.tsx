9f57d7f8e09d610cf7fd1b98de5542d28463d227
s4ze <imgood1104@gmail.com>
2024-02-11 23:43:30 +0300

    Добавление функционала комментариев

--- components/Form.tsx
+++ components/Form.tsx
@@ -34,7 +34,11 @@ const Form: React.FC<FormProps> = ({
     try {
       setIsLoading(true)
 
-      await axios.post('/api/posts', { body })
+      const url = isComment ?
+        `/api/comments?postId=${postId}` :
+        '/api/posts'
+
+      await axios.post(url, { body })
 
       toast.success('Tweet created')
 
@@ -46,7 +50,7 @@ const Form: React.FC<FormProps> = ({
     } finally {
       setIsLoading(false)
     }
-  }, [body, mutatePosts])
+  }, [body, mutatePosts, isComment, postId])
   return (
     <div className="border-b-[1px] border-neutral-800 px-5 py-2">
       {currentUser ? (
