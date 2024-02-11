d3245bd4b858675f8f0739f875365aa07c8015f5
s4ze <imgood1104@gmail.com>
2024-02-12 00:38:06 +0300

    Добавление уведомлений

--- components/layout/SidebarItem.tsx
+++ components/layout/SidebarItem.tsx
@@ -1,6 +1,7 @@
 import { useRouter } from "next/router";
 import { useCallback } from "react";
 import { IconType } from "react-icons";
+import { BsDot } from "react-icons/bs"
 
 import useCurrentUser from "@/hooks/useCurrentUser";
 import useLoginModal from "@/hooks/useLoginModal";
@@ -11,6 +12,7 @@ interface SidebarItemProps {
   icon: IconType;
   onClick?: () => void;
   auth?: boolean;
+  alert?: boolean
 }
 
 const SidebarItem: React.FC<SidebarItemProps> = ({
@@ -18,7 +20,8 @@ const SidebarItem: React.FC<SidebarItemProps> = ({
   href,
   icon: Icon,
   onClick,
-  auth
+  auth,
+  alert
 }) => {
   const loginModal = useLoginModal();
   const { data: currentUser } = useCurrentUser();
@@ -53,6 +56,7 @@ const SidebarItem: React.FC<SidebarItemProps> = ({
         "
       >
         <Icon size={28} color='white' />
+        {alert ? <BsDot className="text-sky-500 absolute -top-4 left-0" size={70} /> : null}
       </div>
       <div
         className="
@@ -72,6 +76,7 @@ const SidebarItem: React.FC<SidebarItemProps> = ({
         <p className="hidden lg:block text-white text-xl">
           {label}
         </p>
+        {alert ? <BsDot className="text-sky-500 absolute -top-4 left-0" size={70} /> : null}
       </div>
     </div>
   );
